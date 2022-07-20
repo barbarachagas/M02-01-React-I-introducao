55fc2a35a52268a8a50de4644d17f5a1c1d84fc7
Barbara Chagas <barbarachagaschrist@gmail.com>
2022-07-20 16:40:27 -0300

    Alterando dados para o nome do projeto

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
 
   return (
     <>
-      <Header>Projeto base para o Módulo React I</Header>
+      <Header>React Hello</Header>
       <Main>
         <p>
           O seu nome é Barbara Chagas, com XX caracteres, e você possui 28 anos.
