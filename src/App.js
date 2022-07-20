d74b9a687c37498f4b86a068d89e8945fdbe30be
Barbara Chagas <barbarachagaschrist@gmail.com>
2022-07-20 17:01:36 -0300

    Alteração no input name, deixando-o dinâmico

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 export default function App() {
-  console.log("Teste no console do navegador");
+  const [name, setName] = useState("Barbara");
+
+  function handleNameChange(event) {
+    setName(event.currentTarget.value);
+  }
 
   return (
     <>
       <Header>React Hello</Header>
       <Main>
+        <div className="flex flex-col my-4">
+          <label htmlFor="input-name" className="text-sm mb-1">
+            Digite o seu nome:
+          </label>
+          <input
+            id="input-name"
+            autoFocus
+            className="border p-1"
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </div>
         <p>
-          O seu nome é Barbara Chagas, com XX caracteres, e você possui 28 anos.
+          O seu nome é {name}, com {name.length} caracteres, e você possui 28
+          anos.
         </p>
       </Main>
     </>
